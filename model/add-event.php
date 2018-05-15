f350313a87dd2e27a7f662d964d26e865f6fcd26
Raine Padilla <epadilla7@mail.greenriver.edu>
2018-05-15 09:43:30 -0400

    writing to & reading from Json file working

--- model/add-event.php
+++ model/add-event.php
@@ -5,25 +5,46 @@
      * Updated: 5/14/18
      * Team: Remote Workers
      */
-    // Retrieve New Event
+
+    //Set Refresh header using PHP.
+    header( "refresh:5;url=../AaronAviles/admin" );
 
     // Send to file upload
-    include "model/file-upload.php";
+    include "file-upload.php";
 
-    $event = array('Name' => $_POST['eventName'], 'Content' => $_POST['eventContent'],
-        'Category' => $_POST['eventCategory']);
+    // Retrieve new event
+    $eventName = $_POST['eventName']; // text.headline
+    $eventDescription = $_POST['eventContent']; // text.text
+    $eventCategory = $_POST['eventCategory']; // append to text.text for now
+    $eventUrl = $_POST['eventUrl']; // media.url
+    $eventUrlCaption = $_POST['urlCaption']; // media.caption
+    // Separate out month, day, year
+    $eventDate = explode("-", $_POST['startDate']); // formatted YYYY/MM/DD
+    $year = $eventDate[0]; // start_date.year
+    $month = $eventDate[1]; // start_date.month
+    $day = $eventDate[2]; // start_date.day
+
+
+    $event = array();
+    $event['text']['headline'] = $eventName;
+    $event['text']['text'] = $eventDescription . " - " . $eventCategory;
+    $event['media']['url'] = $eventUrl;
+    $event['media']['caption'] = $eventUrlCaption;
+    $event['start_date']['month'] = $month;
+    $event['start_date']['day'] = $day;
+    $event['start_date']['year'] = $year;
+
 
     // Pull the contents of the events.json file
     $file = file_get_contents("test_events.json");
 
     // Create a temporary array to hold all events
     $tempEvent = json_decode($file, true);
-    print_r($tempEvent);
 
     // Add the new event to the events array
     $tempEvent[] = $event;
-    print_r($tempEvent);
     // make sure the file is writable
+
     // if it is, write the temporary array back into the file.
-    echo is_writable($file) ? file_put_contents("test_events.json", json_encode($tempEvent)) :
-        "not writable";
+    echo is_writable("test_events.json") ? file_put_contents("test_events.json" . ": Written Successfully!",
+                             json_encode($tempEvent)) : "Something went wrong!";
