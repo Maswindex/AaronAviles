aed349c5276222117c7fab316cdd720b102d4703
Raine Padilla <epadilla7@mail.greenriver.edu>
2018-05-14 20:51:41 -0400

    fixed the adminAdd page, wrong upload. Form still needs to be fixed again and data formatted to test_events.json

--- model/AdminAdd.php
+++ model/AdminAdd.php
@@ -1,9 +1,30 @@
 <?
 if(isset($_POST))
 {
-    $event = array('EventName' => $_POST['eventName'], 'EventContent' => $_POST['eventContent'], 'EventCategory' => $_POST['eventCategory']);
+    /*
+     * Author: Raine Padilla
+     * Date: 4/30/18
+     * Updated: 5/1/18
+     * Team: Remote Workers
+     */
+    // Retrieve New Event
+    $event = array('Name' => $_POST['eventName'], 'Content' => $_POST['eventContent'],
+        'Category' => $_POST['eventCategory']);
 
-    $eventJSon = json_encode($event);
+    // Pull the contents of the events.json file
+    $file = file_get_contents("test_events.json");
+
+    // Create a temporary array to hold all events
+    $tempEvent = json_decode($file, true);
+    print_r($tempEvent);
+
+    // Add the new event to the events array
+    $tempEvent[] = $event;
+    print_r($tempEvent);
+    // make sure the file is writable
+    // if it is, write the temporary array back into the file.
+    echo is_writable($file) ? file_put_contents("test_events.json", json_encode($tempEvent)) :
+        "not writable";
 }
 ?>
 <!--
