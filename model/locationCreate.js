cbb2eb0555c2be4f11935193041283ce8534c999
Toygan Sevim <tsevim@mail.greenriver.edu>
2018-05-22 14:54:06 -0400

    Using Google API to pull data for the searched location.

--- model/locationCreate.js
+++ model/locationCreate.js
@@ -1,3 +1,5 @@
+// Small working copy
+
 google.maps.event.addDomListener(window, 'load', test);
 
 function test() {
@@ -6,15 +8,16 @@ function test() {
         {
             latitude: document.getElementById('latitude'),
             longitude: document.getElementById('longitude'),
-            city: document.getElementById('city')
+            city: document.getElementById('city'),
+            state: document.getElementById('state'),
+            country: document.getElementById('country')
         };
 
-    var input = document.getElementById('autocomplete');
+    var input = document.getElementById('inputLocation');
     var autocomplete = new google.maps.places.Autocomplete(input);
-    //
-    // location['latitude'].data = 'toygan';
-    // location.latitude.html = 'toygan';
-    // , {types: ['(cities)']});
+
+
+    //START THE FUNCTION as the place selected - get the data's
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
 
         var place = autocomplete.getPlace();
@@ -26,77 +29,14 @@ function test() {
         location.latitude.value = place.geometry.location.lat();
         location.city.value = place.name;
         location.longitude.value = place.geometry.location.lng();
+        location.country.value = place.formatted_address;
+        location.state.value = place.address_components[2].short_name;
 
-        document.getElementById("country").innerHTML = address;
+
+
+        // document.getElementsByClassName("address").value += address;
+
+
+        //post the address below
     });
-
-
-    var locationSelected = input;
-    //
-    // const locationFormFields =
-    //     {
-    //         name: $('#location_name'),
-    //         address: $('#location_address'),
-    //         website: $('#location_website'),
-    //         phone: $('#location_phone'),
-    //         hours: $('#location_hours'),
-    //         type: $('#location_type'),
-    //         latitude: $('#location_latitude'),
-    //         longitude: $('#location_longitude'),
-    //         condoms: document.getElementById('location_condoms'),
-    //         hiv: document.getElementById('location_hiv'),
-    //         sti: document.getElementById('location_sti'),
-    //         queer_friendly: document.getElementById('location_queer_friendly'),
-    //         id: $('#location-id'),
-    //
-    //         clear: function () {
-    //             this.name.val('').blur();
-    //             this.address.val('').blur();
-    //             this.website.val('').blur();
-    //             this.phone.val('').blur();
-    //             this.hours.val('').blur();
-    //             this.type.val('').blur();
-    //             this.longitude.val('').blur();
-    //             this.latitude.val('').blur();
-    //             this.condoms.checked = false;
-    //             this.hiv.checked = false;
-    //             this.sti.checked = false;
-    //             this.queer_friendly.checked = false;
-    //             this.id.val('');
-    //         },
-    //
-    //         populate: function (data = {}) {
-    //             if (data.name) this.name.val(data.name).trigger('focus');
-    //             if (data.address) this.address.val(data.address).trigger('focus');
-    //             if (data.website) this.website.val(data.website).trigger('focus');
-    //             if (data.phone) this.phone.val(data.phone).trigger('focus');
-    //             if (data.hours) this.hours.val(data.hours).trigger('focus');
-    //             if (data.type) this.type.val(data.type).trigger('focus');
-    //             if (data.longitude) this.longitude.val(data.longitude).trigger('focus');
-    //             if (data.latitude) this.latitude.val(data.latitude).trigger('focus');
-    //             if (data.condoms) this.condoms.checked = true;
-    //             if (data.hiv) this.hiv.checked = true;
-    //             if (data.sti) this.sti.checked = true;
-    //             if (data.queer_friendly) this.queer_friendly.checked = true;
-    //         },
-    //
-    //         extract: function () {
-    //             return {
-    //                 name: this.name.val(),
-    //                 address: this.address.val(),
-    //                 website: this.website.val(),
-    //                 phone: this.phone.val(),
-    //                 hours: this.hours.val(),
-    //                 type: this.type.val(),
-    //                 longitude: this.longitude.val(),
-    //                 latitude: this.latitude.val(),
-    //                 condoms: this.condoms.checked,
-    //                 hiv: this.hiv.checked,
-    //                 sti: this.sti.checked,
-    //                 queer_friendly: this.queer_friendly.checked
-    //             };
-    //         }
-    //     };
-
-
 }
